35e5c4e0f38bc75449b32317e42a70b1f97eac51
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-09-07 00:33:56 +0000

    saving, i can get a token from POSTING test

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 import os
 
 from flask_migrate import Migrate
@@ -17,7 +20,6 @@ api = Blueprint('api', __name__)
 
 
 
-
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
@@ -26,9 +28,6 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
-
-
-
 
 
 
@@ -72,3 +71,17 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
     return jsonify(request_body_user), 200
+
+#-------------------------------------------------#
+#this is to create a token which will be then saved to the backend for any user that is online - Alvaro C.
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
