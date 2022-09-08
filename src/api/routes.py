6e171ce090300a85ef57424a35bb0cd2e757407a
NahuelPiecora <nahuelpie@live.com>
2022-09-08 22:24:48 +0000

    updated

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Trainerabout
+from api.models import db, User
 from api.utils import generate_sitemap, APIException
 import os
 
